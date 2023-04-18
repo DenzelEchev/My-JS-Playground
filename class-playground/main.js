b6bc0d00d6ee2951bc42b595e5f5c0c41d5a9bee
Denzel M. Echevarria <denzel.echevarria@gmail.com>
2023-04-17 23:06:03 -0400

    added another space to tinker

---
+++ class-playground/main.js
@@ -0,0 +1,10 @@
+class someHTML {
+    constructor(state, theme, html){
+        this.state = state,
+        this.theme = theme,
+        this.html = html
+    }
+    renderH1(){
+        return
+    }
+}
